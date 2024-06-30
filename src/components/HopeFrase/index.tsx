d2c7dc69ea167b6a94302b0e6d34315a756b4775
Breno Tosi <breno_tosi@hotmail.com>
2024-06-29 21:50:46 -0300

    removing logs

--- src/components/HopeFrase/index.tsx
+++ src/components/HopeFrase/index.tsx
@@ -15,11 +15,7 @@ const HopeFrase = () => {
         if (!section) return;
         if (!rectangle) return;
 
-        // const sectionTop = section.getBoundingClientRect().top + window.scrollY;
-        // const sectionHeight = section.offsetHeight;
-
         gsap.to(rectangle, {
-            
             scrollTrigger: {
                 trigger: section,
                 start: 'top center',
@@ -32,32 +28,6 @@ const HopeFrase = () => {
                 }
             }
         })
-
-        // window.addEventListener("scroll", () => {
-        //     const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-
-        //     // Se o scroll atual está antes da seção, definir a largura como 100%
-        //     if (scrollTop < sectionTop - window.innerHeight) {
-        //         rectangle.style.width = '100%';
-        //         return;
-        //     }
-
-        //     // Se o scroll atual está depois da seção, definir a largura como 0%
-        //     if (scrollTop > sectionTop + sectionHeight) {
-        //         rectangle.style.width = '0%';
-        //         return;
-        //     }
-
-        //     // Calcular a porcentagem do scroll dentro da seção, invertida
-        //     const scrollPercentage = .7 - (scrollTop - (sectionTop - window.innerHeight)) / (sectionHeight + window.innerHeight);
-
-        //     const width = Math.min(100, Math.max(0, scrollPercentage * 100));
-        //     console.log(width);
-
-        //     rectangle.style.width = `${width}%`;
-        // });
-
-
     })
     return (
         <section className='hopeFrase'>
