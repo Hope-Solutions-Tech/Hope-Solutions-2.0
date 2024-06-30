a9858d3dda5a078a8cf0d38b9bfbb802e0d2e083
Breno Tosi <breno_tosi@hotmail.com>
2024-06-30 19:56:12 -0300

    ajuste footer mobile e desk

--- src/components/HopeFooter/index.tsx
+++ src/components/HopeFooter/index.tsx
@@ -1,51 +1,53 @@
 import './styles.css'
-import logoFooter from '../../assets/logo-footer.png'
-import linkedinIcon from '../../assets/linkedin.svg'
-import instagramIcon from '../../assets/instagram.svg'
-import telIcon from '../../assets/tel.svg'
-import emailIcon from '../../assets/email.svg'
+import linkedinIcon from '../../assets/linkedin.png'
+import instagramIcon from '../../assets/instagram.png'
+import telIcon from '../../assets/wapp.png'
+import emailIcon from '../../assets/email.png'
+import mockupPhones from '../../assets/two-phones-mockup.png'
 
 const HopeFooter = () => {
     
     return (
         <footer id='hopeFooter'>
             <div className='footerHope'>
-                <h1>Entre em contato</h1>
+                <div className="mockupDiv">
+                    <img src={mockupPhones} alt="" />
+                </div>
                 <div className='footerContent'>
+                    <h1>Revolucione o atendimento de emergência com a <span className='vitseFooterSpan'>Vitse</span></h1>
+                    <button className='contactButton'>
+                        <a href="https://api.whatsapp.com/send?phone=5511976893853&text=Ol%C3%A1" target='blank'>Entre em contato</a>
+                    </button>
                     <ul>
                         <li>
                             <a className='contactLink' href="https://www.instagram.com/hopesolutions_/">
+                                <span>@vitseoficial</span>
                             <img src={instagramIcon}/>
-                                <span>@hopesolutions</span>
                             </a>
                         </li>
                         <li>
                             <a className='contactLink' href="https://www.linkedin.com/company/hope-solutions-health">
+                                <span>vitseoficial</span>
                                 <img src={linkedinIcon} alt="" />
-                                <span>Hope Solutions</span>
                             </a>
                         </li>
                         <li>
                             <a className='contactLink' href="https://api.whatsapp.com/send?phone=5511976893853&text=Ol%C3%A1,%20vim%20pelo%20site%20da%20HopeSolutions">
+                                <span>+55 11 97689-3953</span>
                                 <img src={telIcon} alt="" />
-                                <span>+55 11 99999-9999</span>
                             </a>
                         </li>
                         <li>
                             <a className='contactLink' href="mailto:hopesolutions.tech@gmail.com?subject=Contato%20-%20Hope%20Solutions">
+                                <span>contato@vitse.com.br</span>
                                 <img src={emailIcon} alt="" />
-                                <span>contato@hopesolutions.com.br</span>
                             </a>
                         </li>
                     </ul>
-                    <div className='solutionBy'>
-                        <p>A solution by</p>
-                        <img src={logoFooter} alt="" />
-                    </div>
                 </div>
             </div>
             <p className='footerCopyright'>
-                © Copyright 2024 - HopeSolutions - Todos os direitos reservados - All rights reserved.
+                Copyright © 2024 Vitse | CNPJ - 55.510.736/0001-00
             </p>
         </footer>
     )
